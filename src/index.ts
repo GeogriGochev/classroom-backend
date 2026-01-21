971a7ae0062edad008d8e93f189523a671c3e73f
Georgi Gochev <georgi.gochev@egt.com>
2026-01-21 11:10:29 +0200

    genereate rhe subjects routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import "dotenv/config";
 import express, { type Request, type Response } from "express";
+import subjectsRoute from "./routes/subjects";
+import cors from "cors";
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
 app.use(express.json());
+
+app.use("/api/subjects", subjectsRoute);
 
 app.get("/", (_req: Request, res: Response) => {
   res.json({ message: "Classroom backend is running." });
---
+++ src/routes/subjects.ts
@@ -0,0 +1,69 @@
+import { Router } from "express";
+import { db } from '../db';
+import { subjects, departments } from '../db/schema/app';
+import { and, desc, eq, getTableColumns, like, or, sql } from 'drizzle-orm';
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const { search,department, page= 1, limit= 10 } = req.query
+
+    const currentPage = Math.max(1, +page);
+    const limitPerPage = Math.max(1, +limit);
+    const offset = (currentPage - 1) * limitPerPage;
+    const filterConditions = [];
+
+    // if search query exists filter the subject name or subject code
+    if (search) {
+      filterConditions.push(
+        or(
+          like(subjects.name, `%${search}%`),
+          like(subjects.code, `%${search}%`),
+        )
+      );
+    }
+
+    // if department query exists filter the subject department
+    if (department) {
+      filterConditions.push(
+        like(departments.name, `%${department}%`),
+      );
+    }
+
+    // combine all filters using and if any exist
+    const whereClause = filterConditions.length > 0 ? and(...filterConditions) : undefined;
+    const countResult = await db
+    .select({count: sql<number>`count(*)`})
+    .from(subjects)
+    .leftJoin(departments, eq(subjects.departmentId, departments.id))
+    .where(whereClause);
+
+    const totalCount = countResult[0]?.count || 0;
+
+    const subjectsLists = await db.select({
+      ...getTableColumns(subjects),
+      department: getTableColumns(departments),
+    }).from(subjects).leftJoin(departments, eq(subjects.departmentId, departments.id))
+    .where(whereClause)
+    .orderBy(desc(subjects.id))
+    .limit(limitPerPage)
+    .offset(offset); 
+
+    res.status(200).json({
+      data: subjectsLists,
+      pagination: {
+        page: currentPage,
+        limit: limitPerPage,
+        total: totalCount,
+        totalPages: Math.ceil(totalCount / limitPerPage),
+      }
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+export default router;
