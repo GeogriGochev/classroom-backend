23346933ff50d589d53ea3e1d098775a3693653e
Georgi Gochev <georgi.gochev@egt.com>
2026-01-21 08:59:13 +0200

    chore: ignore node_modules

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { type Request, type Response } from "express";
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
+  res.json({ message: "Classroom backend is running." });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started: http://localhost:${PORT}`);
+});
